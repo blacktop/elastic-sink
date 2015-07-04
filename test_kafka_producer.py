917791d82cda43254dc40fac6ac1873031c3de1a
blacktop <blacktop@users.noreply.github.com>
2015-07-04 00:01:13 -0600

    First Push

---
+++ test_kafka_producer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'blacktop'
+
+import json
+from kafka import SimpleProducer, KafkaClient
+
+# To send messages synchronously
+kafka = KafkaClient('172.16.197.191:9092')
+producer = SimpleProducer(kafka)
+
+tet = dict(this='EEEEEEEEHHHHH!',
+           _is=['one', 'two', 'buckle my shoe, bitch!'],
+           awesome=dict(look_mom='no hands'))
+
+# Note that the application is responsible for encoding messages to type bytes
+producer.send_messages(b'my-topic', b'some message')
+producer.send_messages(b'my-topic', b'this method', b'is variadic')
+producer.send_messages(b'my-topic', json.dumps(tet))
+
+# Send unicode message
+producer.send_messages(b'my-topic', u'你怎么样?'.encode('utf-8'))
